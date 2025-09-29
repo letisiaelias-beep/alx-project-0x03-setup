6724684a4d08e121618acf0346e5b012a9101a38
letisiaelias-beep <letisiaelias@gmail.com>
2025-09-29 12:22:32 +0300

    Update index.tsx

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,4 +1,4 @@
-import Button from "@/components/common/Button";
+import Button from "../components/common/Button";
 import { useRouter } from "next/router";
 import { PageRouteProps } from "@/interfaces"; // we'll handle interfaces later in Task 3
 
